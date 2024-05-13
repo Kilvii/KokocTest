80f7dd52d753631a734889e8bc4cdce71f9191c4
Kirill Ryabikin <98740566+Kilvii@users.noreply.github.com>
2024-05-13 18:34:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://kilvii.github.io/KokocTest/",
+  base: "https://kilvii.github.io/KokocTest/main",
   plugins: [
     vue(),
   ],
